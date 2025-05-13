928de74df7064894ded319edb6a453bf0c59202f
Omid Hashemzadeh <omidh2015@gmail.com>
2025-05-13 15:53:52 +0100

    Add Task Model

--- taskflow_api/settings.py
+++ taskflow_api/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'rest_framework',
+    'drf_yasg',
+    'corsheaders',
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'taskflow_api.urls'
@@ -120,3 +130,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+# MANUALLY ADDED
+
+CORS_ALLOW_ALL_ORIGINS = True
