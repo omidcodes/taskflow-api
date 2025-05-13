f97116ba2a69cd6e73b517899726963f514991a0
Omid Hashemzadeh <omidh2015@gmail.com>
2025-05-13 16:00:06 +0100

    Add CRUD APIs for tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('-created_at')
+    serializer_class = TaskSerializer
