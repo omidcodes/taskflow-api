e3b9dbf88c3bd452c76c1b9eeacac12576bca11f
Omid Hashemzadeh <omidh2015@gmail.com>
2025-05-18 15:44:35 +0100

    Add some filter_backends to ModelViewset

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework import filters
+
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 
@@ -12,3 +14,6 @@ def home_view(request):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by("-created_at")
     serializer_class = TaskSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title"]
